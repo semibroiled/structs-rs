9862cb953d8aed67d85f0b1b6106e6b6c1cafe6c
acm <AmitavCMostafa@gmx.de>
2023-08-18 01:49:09 +0200

    Add methods

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,34 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
+
+    fn square(size: u32) -> Self {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn fit_in(&self, other: &Rectangle) -> bool {
+        self.width < other.width && self.height < other.height
+    }
+}
 fn main() {
     println!("Hello, world!");
 
     let rect1: Rectangle = Rectangle {
-        width: 50,
+        width: dbg!(5 * 10),
         height: 30,
     };
 
-    println!(" The area is {} square meters", area(&rect1)); //call with rect1 reference
-    println!("{:#?}", rect1)
-}
+    let rect2 = Rectangle::square(5);
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!(" The area is {} square meters", rect1.area(&)); //call with rect1 reference
+    println!("{:#?}", rect1);
+
+    println!("Rect 2 can fit in Rect 1 is {}", rect1.fit_in(&rect2));
 }
